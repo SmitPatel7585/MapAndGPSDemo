11e3433096c350446f04464aa175e8ea2052a56c
Smit Patel <smit.patel7585@gmail.com>
2024-03-18 21:29:35 -0400

    Initial Commit

---
+++ LabWork7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabWork7
+//
+//  Created by user235217 on 3/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
